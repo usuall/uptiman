9846d87c688d7807f5e03bbcbcdc4d838228dd85
japansea <usuall@gmail.com>
2022-08-04 15:30:22 +0900

    'test'

--- url2.py
+++ url2.py
@@ -8,9 +8,8 @@ import myfunc
 
 # from mysql_config import dbconn # DB설정
 # cur = dbconn().cursor()
-
-cur = myfunc.dbconn().cursor()
-
+conn = myfunc.dbconn()
+cur = conn.cursor()
 
 # 실행경로
 project_path = os.path.abspath(os.getcwd())
@@ -69,3 +68,4 @@ while 1:
             print ('URL점검 이벤트 발생 : ' + web_url)  #음성 맨트
             #break   #주석 풀면 url_list 처음부터 시작
             
+conn.close()
