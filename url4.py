f8f373dd3bbf088dd705fa85e21691da83c18bd6
japansea <usuall@japansea.net>
2022-08-05 16:52:18 +0900

    url4.py updated

--- url4.py
+++ url4.py
@@ -1,34 +1,66 @@
 import pymysql
 from selenium import webdriver
+from PIL import Image
 import requests
 import urllib3
 import time
 import os
 import myfunc 
 
-def main():
+
+# 실행경로
+project_path = os.path.abspath(os.getcwd())
+lib_path = project_path + '/lib'
+img_path = project_path + '/capture/'
+img_resize_path = project_path + '/capture_resized/'
+
+# 실행환경
+headless = 1 
+user_agent ='user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+
+
+# 이미지 리사이즈(100%->50%)
+def img_resizer(img_path, img_str):    
+    img = Image.open(img_path + img_str)
+    img_half_resize = img.resize((int(img.width / 2), int(img.height / 2)))
+    img_half_resize.save(img_resize_path + 'resized_' + img_str)
     
+
+# 브라우저 기본 설정
+def set_browser_option(options):
+    # '시스템에 부착된 장치가 작동하지 않습니다' 오류 제거
+    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    # 브라우져 창 최대화
+    options.add_argument("--start-maximized")
+    
+    # 브라우져 창 최소화
+    if(headless == 1):
+        options.add_argument('--window-size=900,700')
+        options.add_argument("--headless")       
+    
+    # 실행되는 브라우저 크기를 지정할 수 있습니다.        
+    
+    options.add_argument(user_agent)    
+    
+    # InsecureRequestWarning  메시지 제거
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)    
+    
+
+def main():    
     try:
+        # DB접속
         cur = myfunc.dbconn2()
-        # 실행경로
-        project_path = os.path.abspath(os.getcwd())
-        lib_path = project_path + '/lib'
-        img_path = project_path + '/capture/'        
+        
         # 크롬 브라우저 오픈
         options = webdriver.ChromeOptions()
-        # '시스템에 부착된 장치가 작동하지 않습니다' 오류 제거
-        options.add_experimental_option("excludeSwitches", ["enable-logging"])
-
-        # 브라우져 창 최대화
-        options.add_argument("--start-maximized")
-        # 브라우져 창 최소화
-        #options.add_argument("--headless") 
+        
+        # 브라우져 옵션 설정
+        set_browser_option(options)
         driver = webdriver.Chrome(lib_path + '/chromedriver.exe', chrome_options=options)
-        # InsecureRequestWarning  메시지 제거
-        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)  
-
+        
         # start_url = 'https://www.google.com'
         driver.implicitly_wait(10)
+        driver.set_window_size(1920, 1080)
         # driver.get(start_url)
         
         while 1:
@@ -56,6 +88,7 @@ def main():
                 img_str = str(row['url_no'])+ "__" + row['url_addr'] + ".png"
                 #print (img_str)
                 driver.save_screenshot(img_path + img_str)
+                img_resizer(img_path, img_str)
                 
                 if requests_code != 200 :
                     print ('URL점검 이벤트 발생 : ' + web_url)  #음성 맨트
