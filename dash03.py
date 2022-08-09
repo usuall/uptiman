915ffa55cdff05aa2dbfff92846cd2d8e5fa6754
japansea <usuall@gmail.com>
2022-08-09 15:44:39 +0900

    gui modified

--- dash03.py
+++ dash03.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+from pickle import TRUE
+from threading import TIMEOUT_MAX
 import PySimpleGUI as sg
 import pymysql
 from selenium import webdriver
@@ -31,7 +33,7 @@ mon_status = 0 #모니터링 시작 유무
 # 실행환경
 headless = 0
 user_agent = 'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
-
+# myfont = lib_path + '/font/NanumGothic-Regular.ttf'
 
 # 브라우저 기본 설정
 def set_browser_option(options):
@@ -64,7 +66,7 @@ def my_org_list():
 
 def my_org_list_combo():
 
-    org_list = ['기관 전체']
+    org_list = ['전 체']
     result = get_org_list()
     for row in result:
         #org_list.append(row['org_title']+'['+row['org_no']+']')
@@ -96,10 +98,38 @@ def get_monitoring_all(window):
         window['-OUTPUT-'].update(value=str1, append=True)
         
     
-def getCondition(window):
-    str1 = '[' + get_sysdate() + '] ' +'\n'
-    window['-OUTPUT-'].update(value=str1 + '검색조건 : ' +'\n', append=True)
-    window['-OUTPUT-'].update(value='___________________________________\n', append=True)
+def getCondition(window, values):
+    str1 = '[' + get_sysdate() + '] '
+    print ('condition : '+values['-ORG_LIST-'])
+    if(values['-TIMEOUT1-'] == True ):
+        timeout_term = 5
+    elif(values['-TIMEOUT2-'] == True ):
+        timeout_term = 10
+    elif(values['-TIMEOUT3-'] == True ):
+        timeout_term = 15
+    elif(values['-TIMEOUT4-'] == True ):
+        timeout_term = 20
+    elif(values['-TIMEOUT5-'] == True ):
+        timeout_term = 25
+    elif(values['-TIMEOUT6-'] == True ):
+        timeout_term = 30
+    else:
+        timeout_term = 12
+
+    print ('condition : ' + str(timeout_term))
+
+    
+    window['-OUTPUT-'].update(value='- 실행시간 : ' + str1 + '\n', append=True)
+    window['-OUTPUT-'].update(value='<검색조건>' + '\n', append=True)
+    window['-OUTPUT-'].update(value='- 기관 선택 : ' + values['-ORG_LIST-'] + '\n', append=True)
+    window['-OUTPUT-'].update(value='- 사이트명 : ' + values['-SITE_TITLE-'] + '\n', append=True)
+    window['-OUTPUT-'].update(value='- URL명 : ' + values['-SITE_URL-'] + '\n', append=True)
+    window['-OUTPUT-'].update(value='- 반복 점검 : ' + str(values['-REPEAT-']) + '\n', append=True)
+    window['-OUTPUT-'].update(value='- 비활성화 URL포함. : ' + str(values['-DISABLED-']) + '\n', append=True)
+    window['-OUTPUT-'].update(value='- 백그라운드 실행 : ' + str(values['-BG_EXE-']) + '\n', append=True)
+    window['-OUTPUT-'].update(value='- 타임아웃 설정 : ' + str(timeout_term) + '초\n', append=True)
+    
+    window['-OUTPUT-'].update(value='                                       \n', append=True)
     
 
 def main():
@@ -111,31 +141,22 @@ def main():
     # GUI 실행
     sg.theme('TanBlue')
 
-    column1 = [
-        [sg.Text('Column 1', background_color=sg.DEFAULT_BACKGROUND_COLOR,
-                 justification='center', size=(10, 1))],
-        [sg.Spin(values=('Spin Box 1', '2', '3'),
-                 initial_value='Spin Box 1', key='spin1')],
-        [sg.Spin(values=('Spin Box 1', '2', '3'),
-                 initial_value='Spin Box 2', key='spin2')],
-        [sg.Spin(values=('Spin Box 1', '2', '3'), initial_value='Spin Box 3', key='spin3')]]
-
     layout = [
-        [sg.Text('Uptime HealthCheck Manager', size=(30, 1), font=("Helvetica", 25))],
-        [sg.Text('URL 모니터링 툴입니다. 실행전에 아래 설정부분을 선택하고 실행바랍니다.')],
+        [sg.Text('Uptime Manager', size=(30, 1), font=("Helvetica", 25))],
+        [sg.Text('URL 모니터링 툴입니다. 조건을 선택하고 실행하세요')],
         # [sg.InputText('', key='in1')],
         # [sg.Listbox(values=(org_list), size=(30, 1), key='-ORG_LIST-', enable_events=True)],
         [sg.Text('기관 선택'), sg.Combo(values=(org_list), size=(30, 1), key='-ORG_LIST-', enable_events=True)],
-        [sg.Text(' 사이트명'), sg.InputText('', key='-SITE_TITLE-', size=(25, 1)),sg.Text('  URL'), sg.InputText('', key='-SITE_URL-', size=(25, 1))],
+        [sg.Text(' 사이트명'), sg.InputText('', key='-SITE_TITLE-', size=(30, 1)),sg.Text('  URL'), sg.InputText('', key='-SITE_URL-', size=(30, 1))],
         [sg.CBox('반복 점검', key='-REPEAT-', default=True), sg.CBox('비활성화 URL 포함', key='-DISABLED-'), sg.CBox('백그라운드 실행', key='-BG_EXE-')],
-        [sg.Text('타임아웃'), sg.Radio('5초', "v_timeout", key='timeout1'),
-                            sg.Radio('10초', "v_timeout", key='timeout2', default=True),
-                            sg.Radio('15초', "v_timeout", key='timeout3'),
-                            sg.Radio('20초', "v_timeout", key='timeout4'),
-                            sg.Radio('25초', "v_timeout", key='timeout5'),
-                            sg.Radio('30초', "v_timeout", key='timeout6')],
+        [sg.Text('타임아웃'), sg.Radio('5초',  group_id="RADIO1", key='-TIMEOUT1-'),
+                            sg.Radio('10초', group_id="RADIO1", default=True, key='-TIMEOUT2-'),
+                            sg.Radio('15초', group_id="RADIO1", key='-TIMEOUT3-'),
+                            sg.Radio('20초', group_id="RADIO1", key='-TIMEOUT4-'),
+                            sg.Radio('25초', group_id="RADIO1", key='-TIMEOUT5-'),
+                            sg.Radio('30초', group_id="RADIO1", key='-TIMEOUT6-')],
 
-        [sg.MLine(default_text='LOG AREA\n', font='Lucida', size=(80, 20), key='-OUTPUT-', autoscroll=True, disabled=True)],
+        [sg.MLine(default_text='', font='Gothic', size=(80, 20), key='-OUTPUT-', autoscroll=True, disabled=True)],
         # [sg.Combo(('Combobox 1', 'Combobox 2'), key='combo', size=(20, 1)),sg.Slider(range=(1, 100), orientation='h', size=(34, 20), key='slide1', default_value=85)],
         # [sg.Combo(('Combobox 1', 'Combobox 2'), key='combo', size=(20, 1))],
         # [sg.OptionMenu(('Menu Option 1', 'Menu Option 2', 'Menu Option 3'), key='optionmenu')],
@@ -144,10 +165,11 @@ def main():
         # [sg.Text('Your Folder', size=(15, 1), justification='right'),
         # sg.InputText('Default Folder', key='folder'), sg.FolderBrowse()],
         [sg.Button('종 료', key='-BUTTON_EXIT-', button_color=('white', 'firebrick3')),
-         sg.Text(' ' * 50), sg.Button('     시 작     ', key='-BUTTON_START-'), sg.Button('중 지', key='-BUTTON_STOP-', button_color=('white', 'firebrick3'))]
+         sg.Text('  ' * 35), sg.Button('     실 행     ', key='-BUTTON_START-'), sg.Button('중 지', key='-BUTTON_STOP-', disabled=True, button_color=('black', 'lightblue'))]
     ]
 
-    window = sg.Window('Uptime Health Check Manager for NIRS', layout, default_element_size=(40, 1),
+    obj_list = '-ORG_LIST-', '-TIMEOUT1-', '-TIMEOUT2-', '-TIMEOUT3-', '-TIMEOUT4-', '-TIMEOUT5-', '-TIMEOUT6-', '-DISABLED-', '-SITE_TITLE-', '-SITE_URL-', '-BG_EXE-', '-BUTTON_START-', '-BUTTON_EXIT-'
+    window = sg.Window('Uptime Manager for NIRS', layout, default_element_size=(40, 1),
                        grab_anywhere=False)
 
     while True:
@@ -155,15 +177,21 @@ def main():
 
 
 
+
         if event == '-BUTTON_START-':
             print('시작')
-            
-            #조회 조건
-            getCondition(window)
+            window['-OUTPUT-'].update(value='', append=False)
+            # 버튼 활성화 전환
+            window['-BUTTON_STOP-'].update(disabled=False)
+            for key in obj_list:
+                window[key].update(disabled=True)
+
+            #조회조건 출력
+            getCondition(window, values)
             
             mon_status = 1
             org_title = values['-ORG_LIST-']
-            if org_title == '기관 전체':
+            if org_title == '전 체':
                 print('전체 점검..... ')
                 get_monitoring_all(window)
 
@@ -181,12 +209,14 @@ def main():
             # save(values)
         elif event == '-BUTTON_STOP-':
             print('중지')
-            # filename = sg.popup_get_file('Load Settings', no_window=True)
-            # window.LoadFromDisk(filename)
-            # load(form)
-        elif event in ('-BUTTON_EXIT-', None):
+            # 버튼 활성화 전환
+            window['-BUTTON_STOP-'].update(disabled=True)
+            for key in obj_list:
+                window[key].update(disabled=False)
+
+        elif event in ('-BUTTON_EXIT-', 'Escape:27', sg.WIN_CLOSED):
             # Todo : browser & dbconnection close.
-            
+            print('exit....')
             break
 
     window.close()
